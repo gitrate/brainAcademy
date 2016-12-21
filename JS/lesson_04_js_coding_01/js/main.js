07f4420a8fe3484943dc65d7c13fe80a090129e1
gitrate <htmlfau@gmail.com>
2016-12-21 20:59:59 +0200

    js intro

---
+++ JS/lesson_04_js_coding_01/js/main.js
@@ -0,0 +1,65 @@
+"use strict";
+// alert("Hello, world");
+// console.log("Hello, World!");
+// debugger;
+// var x = "My x";
+// var x = "My x";
+// x = "Another x";
+// console.log(x);
+
+// var x = 123;
+// console.log(typeof x);
+
+// var x = "string";
+// console.log(typeof x);
+
+// var x = true;
+// console.log(typeof x);
+
+// var x = null;
+// console.log(typeof x);
+
+// var x = undefined;
+// console.log(typeof x); // "undefined"
+
+// var x = {
+	// key1: "value1",
+	// key2: "value2",
+	// "key3": "value3"
+// };
+ // x.key1 = "new value";
+ // x["key2"] = "new value 2";
+
+ 
+// console.log(typeof x); // "object"
+
+// var x = ["element1", "element2", "element3", 25, {}, [], null];
+// console.log((Array.isArray(x))); // "object"
+// x[3] = 56;
+// console.log(x[3]));
+// console.log(typeof x);
+// Array.isArray(x);
+
+// var x = function () {};
+// x.key1 = "test";
+
+// console.log(x.key1);
+// console.log(typeof x);
+
+var x = ["element1", "element2", "element3", 25, {}, [], null];
+x.test = 123;
+consolel.log(x.key1);
+consolel.log(typeof x);
+
+
+
+
+
+
+
+
+
+
+
+
+
